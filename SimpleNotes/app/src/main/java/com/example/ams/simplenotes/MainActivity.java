f02516e248ca37d91ade58661ac23ea24ffadc82
Appms <amserik@hotmail.com>
2015-04-13 18:13:48 +0200

    MainActivity - Interface

--- SimpleNotes/app/src/main/java/com/example/ams/simplenotes/MainActivity.java
+++ SimpleNotes/app/src/main/java/com/example/ams/simplenotes/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.ams.simplenotes;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+import android.widget.SimpleExpandableListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity implements INotesView {
+
+    private static final String TOPIC_LIST_HEADER = "Topic_header";
+    private static final String TOPIC_LIST_HEADER_NAME = "Topics";
+    private static final String TOPIC_GROUP_HEADER = "Topic_group";
 
     private ViewModel viewModel;
+    private ListView notes;
+    private ExpandableListView topics;
+    private List<Map<String, String>> groupForTopics;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +39,85 @@ public class MainActivity extends Activity {
         String[] initial_topics = this.getResources().getStringArray(R.array.initial_topics);
         NotesDatabase myDBNotes = NotesDatabase.getInstance(this);
         NotesModel notesModel = NotesModel.getInstance(myDBNotes, initial_topics);
+
         viewModel = viewModel.getInstance(notesModel);
+        notes = (ListView) findViewById(R.id.notesForTopicsList);
+        topics = (ExpandableListView) findViewById(R.id.topicsList);
+
+        groupForTopics = new ArrayList<Map<String, String>>() {{
+            add(new HashMap<String, String>() {{
+                put(TOPIC_LIST_HEADER, TOPIC_LIST_HEADER_NAME);
+            }});
+        }};
+
+        topics.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                HashMap<String, String> topicItem = (HashMap<String, String>)
+                        parent.getExpandableListAdapter().getChild(groupPosition,
+                                childPosition);
+                String topicItemName = topicItem.get(TOPIC_GROUP_HEADER);
+                viewModel.onTopicsListPressed(groupPosition, topicItemName);
+                return true;
+            }
+        });
+
+        notes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                Object noteItem = notes.getItemAtPosition(position);
+                viewModel.onNotesListPressed(position, noteItem.toString());
+            }
+        });
+
     }
 
+    public void fillListOfNotes(ArrayList<String> listOfNotes){
+        ArrayAdapter<String> adapterForNotes = new ArrayAdapter<String>(this, R.layout.list_item, listOfNotes);
+        notes.setAdapter(adapterForNotes);
+    }
+
+    @Override
+    public void switchToNoteEdition() {
+
+    }
+
+    @Override
+    public void askForATopic() {
+
+    }
+
+    @Override
+    public void setTopic(String topic) {
+
+    }
+
+    @Override
+    public void setTopicBgColor(String bgColor) {
+
+    }
+
+    public void fillListOfTopics(ArrayList<String> listOfTopics) {
+
+        List<List<Map<String,String>>> finalListOfTopics =
+                new ArrayList<List<Map<String, String>>>();
+        List<Map<String, String>> group = new ArrayList<Map<String, String>>();
+        HashMap<String, String> item;
+
+         SimpleExpandableListAdapter adapterForTopics = new SimpleExpandableListAdapter(this, groupForTopics,
+                R.layout.topic_group, new String[]{TOPIC_LIST_HEADER},
+                new int[]{R.id.TopicListHeader}, finalListOfTopics,
+                R.layout.topic_item, new String[]{TOPIC_GROUP_HEADER},
+                new int[]{R.id.TopicListItem});
+        topics.setAdapter(adapterForTopics);
+    }
+
+    @Override
+    public void collapseTopic(int groupPos) {
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ SimpleNotes/app/src/main/java/com/example/ams/simplenotes/NotesModel.java
@@ -0,0 +1,7 @@
+package com.example.ams.simplenotes;
+
+/**
+ * Created by AMS on 13/04/2015.
+ */
+public class NotesModel {
+}
